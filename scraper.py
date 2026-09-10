c371b9786b17fb984b03d0a5b85febc732b4d0ad
serdark <askarakurt7@gmail.com>
2026-09-11 00:01:22 +0300

    Add 'klinik' to keywords in scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 # "üniversitesi" kelimesini de test için ekledim.
 KEYWORDS = [
     "görsel", "görsel iletişim", "iletişim tasarım", "iletişim ve tasarımı", 
-    "iletişim tasarımı", "grafik", "gastronomi", "mutfak sanatları", "üniversitesi"
+    "iletişim tasarımı", "grafik", "gastronomi", "mutfak sanatları", "klinik"
 ]
 
 def send_telegram_message(message):
